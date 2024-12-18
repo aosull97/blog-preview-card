1bd7a194fb25ec018391a2c5e2be1e35f0ad4a41
Alice <aosullivan@netpremacy.com>
2024-12-18 14:58:31 +0000

    custom colours and fonts added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/*.html"],
   
   theme: {
-    extend: {},
+    colors: {
+      'yellow':'#F4D04E',
+      'gray-950':'#111111',
+      'gray-500':'#6B6B6B'
+    },
+    extend: {
+      fontFamily: {
+        'figtree': ['Figtree', 'serif']
+      }
+
+    },
   },
   plugins: [],
+  safelist: [
+    'bg-yellow',
+    'font-figtree'
+  ]
+
 }
 
