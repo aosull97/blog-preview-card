f0f650adf516930c664b63d6e67e0e4af0cdefb4
Alice <aosullivan@netpremacy.com>
2024-12-19 10:35:32 +0000

    Tailwind reinstalled and custom colours added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    colors: {
+      'yellow':'#F4D04E',
+      'gray-950':'#111111',
+      'gray-500':'#6B6B6B',
+      'white':'#FFFFFF'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
